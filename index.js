56ab238e08662bc7540e2e638305ed60e2d89534
Yasir Al Shekerchi <yalshekerchi@outlook.com>
2018-08-02 23:18:29 -0400

    Updated project settings to work correctly on production server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const keys = require('./config/keys');
 
@@ -23,6 +24,17 @@ require('./routes')(app);
 require('./routes/scrapeRoutes')(app);
 require('./routes/apiRoutes')(app);
 
-// Listen to port 5000
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve producution assets, ie. main.js, main.css
+  app.use(express.static('client/build'));
+
+  // If unknown route, serve index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Listen to appropriate port based on environment
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
